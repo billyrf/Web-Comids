3312fcbb2d6e1655c307779b5d53cbdea4ef1be7
billyrf <billym@gec.inatel.br>
2019-09-28 19:39:21 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { GridModule } from './app-shell/register/grid.module';
 import { BlankModule } from './app-shell/homePage/blank.module';
 import { NavBarComponent } from './app-shell/nav-bar/nav-bar.component';
 import { FooterComponent } from './app-shell/footer/footer.component';
-import { HomeIndexComponent } from './app-shell/homeIndex/home-index.component';
+
 
 @NgModule({
   declarations: [
