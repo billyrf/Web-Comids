8cf452d3138527013a3500b1a2ce112d3713d39f
billyrf <billym@gec.inatel.br>
2019-09-28 17:58:44 -0300

    3 files modified

--- src/app/app-shell/master-detail/master-detail.component.ts
+++ src/app/app-shell/master-detail/master-detail.component.ts
@@ -3,7 +3,7 @@
 @Component({
   selector: 'app-master-detail',
   templateUrl: './master-detail.component.html',
-  styleUrls: ['./master-detail.component.css']
+  styleUrls: ['./master-detail.component.css','../homePage/blank.materialize.min.css','../homePage/blank.materialize.css']
 })
 export class MasterDetailComponent implements OnInit {
 
