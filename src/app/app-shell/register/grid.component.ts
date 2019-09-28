6603b1492dc892b6047f1c543210b69cb445275c
billyrf <billym@gec.inatel.br>
2019-09-28 16:18:43 -0300

    6 files added, 12 deleted

---
+++ src/app/app-shell/register/grid.component.ts
@@ -0,0 +1,21 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-grid',
+  templateUrl: './grid.component.html',
+  styleUrls: ['./grid.component.css']
+})
+export class GridComponent implements OnInit {
+
+  GreyBox = require('../../../assets/GreyBox.svg') as string;
+  WarningMessageText = 'Request to get grid text failed:';
+  WarningMessageOpen = false;
+  ngOnInit() {
+    
+  }
+  handleWarningClose(open: boolean) {
+    this.WarningMessageOpen = open;
+    this.WarningMessageText = '';
+  }
+}
