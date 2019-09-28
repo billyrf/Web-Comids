e2b33d53c94adf65f1e8cd15871614143972c9b7
billyrf <billym@gec.inatel.br>
2019-09-28 17:05:35 -0300

    4 files modified, 2 deleted

--- src/app/app-shell/list/list.component.ts
+++ src/app/app-shell/list/list.component.ts
@@ -1,54 +1,19 @@
-﻿import { Component, OnInit } from '@angular/core';
-
-import { ListService, IListItem } from './list.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css' , '../homePage/blank.materialize.min.css','../homePage/blank.materialize.css']
 })
 export class ListComponent implements OnInit {
-  listItems: IListItem[] = [];
+
   WarningMessageText = 'Request to get list items failed:';
   WarningMessageOpen = false;
-  constructor(private listService: ListService) { }
+
 
   ngOnInit() {
-    this.listService.getListItems().subscribe(
-      response => {
-        this.listItems = response;
-      },
-      error => {
-        this.WarningMessageOpen = true;
-        this.WarningMessageText = `Request to get list items failed: ${error}`;
-      }
-    );
   }
 
-  handleAddListItem(inputText: string) {
-    this.listService.addListItem(inputText).subscribe(
-      (response) => {
-        this.listItems.splice(0, 0, response);
-      },
-      error => {
-        this.WarningMessageOpen = true;
-        this.WarningMessageText = `Request to add list item failed: ${error}`;
-      }
-    );
-  }
-
-  handleDeleteListItem(id: number) {
-
-    this.listService.deleteListItem(id).subscribe(
-      response => {
-        this.listItems = this.listItems.filter(item => item._id !== response._id);
-      },
-      error => {
-        this.WarningMessageOpen = true;
-        this.WarningMessageText = `Request to delete list item failed: ${error}`;
-      }
-    );
-  }
   handleWarningClose(open: boolean) {
     this.WarningMessageOpen = open;
     this.WarningMessageText = '';
