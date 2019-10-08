7ef730c039d7f643386d83e2bfe5253d9e83646d
billyrf <billym@gec.inatel.br>
2019-10-07 22:28:24 -0300

    2 files modified

--- src/app/app-shell/listPlace/list.component.ts
+++ src/app/app-shell/listPlace/list.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css' , '../homePage/blank.materialize.min.css','../homePage/blank.materialize.css']
+  styleUrls: ['./list.component.css' , '../homePage/blank.materialize.css']
 })
 export class ListComponent  {
 
